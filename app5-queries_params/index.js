9d9c2242273cb60645ff3e0b8c13bc19e37b6dd0
loren pabst <loren pabst>
2017-05-12 10:58:29 -0600

    most of step 5 is done

--- app5-queries_params/index.js
+++ app5-queries_params/index.js
@@ -7,6 +7,46 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/api/getData', function(req, res, next){
+	let q = req.query;
+
+	if (req.query){
+		var filteredData = data.filter(function(obj){
+			for (var key in q){
+				if (obj[key] !== q[key]){
+					return false;
+				}
+			}
+			return true;
+		})
+		return res.status(200).send(filteredData);
+	}else{
+		return res.status(200).send(data);
+	}
+	
+});
+app.get('/api/getItem/:index', function(req, res, next){
+	let index = req.params.index;
+	return res.status(200).json(data[index]);
+});
+
+app.put('/api/updateItem/:index', function(req, res, next){
+	let index = req.params.index;
+
+});
+
+app.post('/api/getData', function(req, res, next){
+	let newPerson = req.body.newPerson;
+	data.push(newPerson);
+	return res.status(200).send('ok');
+});
+
+app.delete('/api/delete/:index', function(req, res, next){
+	let index = req.params.index;
+	data.splice(index, 1);
+	return res.status(200).send('ok');
+});
+
 
 
 
