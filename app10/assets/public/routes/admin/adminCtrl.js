4a904d534f24e8094e284d0fdf4750ff5a83d96d
loren pabst <loren pabst>
2017-05-12 16:49:25 -0600

    working on get users functions

--- app10/assets/public/routes/admin/adminCtrl.js
+++ app10/assets/public/routes/admin/adminCtrl.js
@@ -1,8 +1,19 @@
 angular.module('fullstack').controller('adminCtrl', function($scope, userService){
 
   
+    $scope.results = '';
   
+    $scope.getUsers = function(){
+        userService.getUsers().then(function(response){
+            console.log(response);
+        })
+    }
 
+    $scope.getUserById = function(id){
+        userService.getUserById(id).then(function(response){
+            console.log(response);
+        })
+    }
 
 
 
--- app10/assets/public/routes/home/homeCtrl.js
+++ app10/assets/public/routes/home/homeCtrl.js
@@ -4,17 +4,7 @@ angular.module('fullstack').controller('homeCtrl', function($scope, userService)
     $scope.admin = '';
     $scope.status = '';
 
-    $scope.getUsers = function(){
-        userService.getUsers().then(function(response){
-            console.log(response);
-        })
-    }
 
-    $scope.getUserById = function(id){
-        userService.getUserById(id).then(function(response){
-            console.log(response);
-        })
-    }
 
     $scope.login = function(userObj){
         userService.login(userObj).then(function(response){
